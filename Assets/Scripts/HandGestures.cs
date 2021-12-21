a9432ef15d8149b5cac4a8c1531f704ce099c555
Dilmer Valecillos <dilmervst@gmail.com>
2021-12-21 13:32:36 -0700

    Added passthrough demo with hands

---
+++ Assets/Scripts/HandGestures.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HandGestures : MonoBehaviour
+{
+    [SerializeField]
+    private OVRHand ovrHand;
+
+    [SerializeField]
+    private OVRHand.Hand HandType = OVRHand.Hand.None;
+
+    private void OnEnable()
+    {
+        if(ovrHand == null)
+        {
+            Logger.Instance.LogError("ovrHand must be set in the inspector...");
+        }
+        else
+        {
+            Logger.Instance.LogInfo("ovrHand was set correctly in the inspector...");
+        }
+    }
+    void Update()
+    {
+        if(ovrHand.GetFingerIsPinching(OVRHand.HandFinger.Index))
+        {
+            Logger.Instance.LogInfo($"Hand ({HandType}) Pinch Strength ({ovrHand.GetFingerPinchStrength(OVRHand.HandFinger.Index)})");
+        }
+    }
+}
